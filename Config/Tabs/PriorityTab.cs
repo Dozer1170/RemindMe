ca9cf5076726effb69dddfe089f40101f91625fa
Eric Schlichting <ericschlichting@gmail.com>
2021-11-06 12:10:31 -0500

    Priority order works for cooldown timers

--- Config/Tabs/PriorityTab.cs
+++ Config/Tabs/PriorityTab.cs
@@ -2,6 +2,7 @@
 using System.Numerics;
 using ImGuiNET;
 using RemindMe.Config;
+using Action = Lumina.Excel.GeneratedSheets.Action;
 
 namespace RemindMe 
 {
@@ -21,15 +22,23 @@ namespace RemindMe
 
         private void DrawDisplayPriority(MonitorDisplay display)
         {
+            ImGui.Indent(10);
             ImGui.Checkbox("Enable priority sort", ref display.SortByPriority);
 
             if (!display.SortByPriority) return;
             
-            ImGui.BeginListBox("", new Vector2(ImGui.GetWindowWidth(), 118f));
-            foreach (var cooldown in display.Cooldowns)
+            ImGui.BeginListBox("", new Vector2(ImGui.GetWindowWidth() - 10f, 118f));
+            
+            for (var i = 0; i < display.Cooldowns.Count; i++)
             {
+                var cooldown = display.Cooldowns[i];
                 var action = plugin.ActionManager.GetAction(cooldown.ActionId);
                 if (action == null) continue;
+
+                if (!display.SortPriorities.ContainsKey(action.Name))
+                {
+                    display.SortPriorities[action.Name] = i;
+                }
                 
                 var icon = plugin.IconManager.GetActionIcon(action);
                 if (icon != null) {
@@ -42,18 +51,38 @@ namespace RemindMe
                 ImGui.Text(action.Name);
 
                 ImGui.SameLine(ImGui.GetWindowWidth() - 80);
-                if (ImGui.ArrowButton("Up", ImGuiDir.Up))
+                if (ImGui.ArrowButton($"Up{i}", ImGuiDir.Up) && i > 0)
                 {
-                    // Move action up            
+                    // Move action up       
+                    Swap(display, action, i, i - 1);
+                    Save();
                 }
                 
                 ImGui.SameLine(ImGui.GetWindowWidth() - 50);
-                if (ImGui.ArrowButton("Down", ImGuiDir.Down))
+                if (ImGui.ArrowButton($"Down{i}", ImGuiDir.Down) && i < display.Cooldowns.Count - 1)
                 {
                     // Move action down
+                    Swap(display, action, i, i + 1);
+                    Save();
                 }
             }
             ImGui.EndListBox();
+        }
+
+        private void SwapActionPriorities(MonitorDisplay display, Action action, int index, int targetIndex)
+        {
+            var targetSlotAction = plugin.ActionManager.GetAction(display.Cooldowns[targetIndex].ActionId);
+            if (targetSlotAction == null)
+                return;
+
+            display.SortPriorities[action.Name] = targetIndex;
+            display.SortPriorities[targetSlotAction.Name] = index;
+        }
+
+        private void Swap(MonitorDisplay display, Action action, int index, int targetIndex)
+        {
+            SwapActionPriorities(display, action, index, targetIndex);
+            (display.Cooldowns[targetIndex], display.Cooldowns[index]) = (display.Cooldowns[index], display.Cooldowns[targetIndex]);
         }
     }
 }
